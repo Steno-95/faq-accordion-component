4e67062b362dfdbf7d17c53a87caa95efed88fd4
Steno-95 <lezzistefano95@gmail.com>
2025-05-09 21:42:13 +0200

    Feat: added footer component

---
+++ src/ui/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <footer className="text-center text-[.7rem] text-gray-400 mt-10 min-w-fit">
+      Challenge by{" "}
+      <a
+        className="text-(--purple-dark) hover:text-(--purple) focus:text-(--purple) hover:font-semibold"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        className="text-(--purple-dark) hover:text-(--purple) focus:text-(--purple) hover:font-semibold"
+        href="https://github.com/Steno-95"
+        title="The link will redirect to the github of Stefano Lezzi"
+      >
+        Stefano Lezzi
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
